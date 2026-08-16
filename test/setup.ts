96344acf537030c69bb6acba26901fee08cc3471
Ferhat Akköprü <akkopru_ferhat65@outlook.com>
2026-08-16 13:19:24 +0300

    chore: clean up vscode mock import & ignore test files in vsix bundle

--- test/setup.ts
+++ test/setup.ts
@@ -1,21 +1,5 @@
 import Module from 'node:module';
-
-const mockVscode = {
-    window: {
-        showWarningMessage: async () => undefined,
-        showInformationMessage: async () => undefined,
-        showErrorMessage: async () => undefined,
-        createOutputChannel: () => ({
-            appendLine: () => {},
-            show: () => {},
-            dispose: () => {},
-        }),
-    },
-    Uri: {
-        parse: (str: string) => ({ toString: () => str, scheme: 'remoteforge', path: str }),
-        file: (str: string) => ({ toString: () => str, scheme: 'file', path: str }),
-    },
-};
+import * as mockVscode from './mocks/vscode.js';
 
 const originalRequire = (Module.prototype as any).require;
 (Module.prototype as any).require = function (id: string) {
